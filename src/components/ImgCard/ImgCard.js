6eab1bd3b24d60af591d0e5e77d545c21bd84b18
emilybaum <emily.r.baum@gmail.com>
2019-08-30 21:16:40 -0400

    added more components for all parts of the page

---
+++ src/components/ImgCard/ImgCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function ImgCard() {
+    return (
+        <div className="card">
+            <img src="https://bit.ly/2LhC6xO" className="card-img-top" alt="#"/>
+        </div>
+    )
+} 
+
+export default ImgCard;
