9e5f2abd81c2c2bafbd956403bc07456861f41b3
emilybaum <emily.r.baum@gmail.com>
2019-09-04 06:09:17 -0400

    reverted the react readme

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -7,7 +7,7 @@ function Instructions() {
         <div>
             <div className="jumbotron jumbotron-fluid mt-5">
                 <div className="container">
-                    <h1 className="display-3 eb-title-font">Clicky memory</h1>
+                    <h1 className="display-3 ">Clicky memory</h1>
                     <p className="lead">Save all 16 species, but be sure not to select the same one twice. If you click the same animal twice, you lose.</p>
                 </div>
             </div>
